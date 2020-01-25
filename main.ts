a59b6c1979bcd169238baf256fc34454933f2821
carlcst <Carl.stagg@stagg.org>
2020-01-24 21:09:40 -0500

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,14 @@
+let range = 0
 forever(function () {
     pixel.setColor(0x7f00ff)
+    while (range < 1023) {
+        pins.LED.analogWrite(range)
+        pins.LED.analogSetPeriod(300)
+        range += 1
+    }
+    while (range > 0) {
+        pins.LED.analogWrite(range)
+        pins.LED.analogSetPeriod(300)
+        range += -1
+    }
 })
